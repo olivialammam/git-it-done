cbc4aa528e29f031c13c218a6438ad21378b706a
Olivia Lammam <olivia.l7475@gmail.com>
2022-04-11 15:34:12 -0700

    added buttons

--- assets/css/js/single.js
+++ assets/css/js/single.js
@@ -1,7 +1,6 @@
 var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
-var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoName = function() {
   // grab repo name from url query string
@@ -19,29 +18,27 @@ var getRepoName = function() {
   }
 };
 
-
 var getRepoIssues = function(repo) {
   // format the github api url
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
   // make a get request to url
-// make a get request to url
-fetch(apiUrl).then(function(response) {
-  // request was successful
-  if (response.ok) {
-    response.json().then(function(data) {
-      displayIssues(data);
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayIssues(data);
 
-      // check if api has paginated issues
-      if (response.headers.get("Link")) {
-        displayWarning(repo);
-      }
-    });
-  } else {
-    // if not successful, redirect to homepage
-    document.location.replace("./index.html");
-  }
-});
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo);
+        }
+      });
+    } else {
+      // if not successful, redirect to homepage
+      document.location.replace("./index.html");
+    }
+  });
 };
 
 var displayIssues = function(issues) {
@@ -57,12 +54,11 @@ var displayIssues = function(issues) {
     issueEl.classList = "list-item flex-row justify-space-between align-center";
     issueEl.setAttribute("href", issues[i].html_url);
     issueEl.setAttribute("target", "_blank");
-    
+
     // create span to hold issue title
     var titleEl = document.createElement("span");
     titleEl.textContent = issues[i].title;
-    
-  
+
     // append to container
     issueEl.appendChild(titleEl);
 
@@ -72,14 +68,13 @@ var displayIssues = function(issues) {
     // check if issue is an actual issue or a pull request
     if (issues[i].pull_request) {
       typeEl.textContent = "(Pull request)";
-    }
-    else {
+    } else {
       typeEl.textContent = "(Issue)";
     }
 
     // append to container
     issueEl.appendChild(typeEl);
-  
+
     // append to the dom
     issueContainerEl.appendChild(issueEl);
   }
@@ -99,5 +94,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
 getRepoName();
